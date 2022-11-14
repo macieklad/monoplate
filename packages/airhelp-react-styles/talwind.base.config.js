ecb0a5bbb6b5ad2346720921a46d004246db551b
Maciej Ładoś <maciej.lados@airhelp.com>
2022-11-14 16:29:42 +0100

    feat: company font and screen setup in tailwind

--- packages/airhelp-react-styles/talwind.base.config.js
+++ packages/airhelp-react-styles/talwind.base.config.js
@@ -1,6 +1,29 @@
 module.exports = {
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: [
+        'AirHelp',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
   },
   plugins: [],
 };
