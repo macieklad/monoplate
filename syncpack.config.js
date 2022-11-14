3a0ec04511cbc0c005b3482829712cccd2f2d3f5
Maciej Ładoś <maciej.lados@airhelp.com>
2022-11-14 15:22:57 +0100

    fix: ignore airhelp wildcard packages in synpack

--- syncpack.config.js
+++ syncpack.config.js
@@ -1,12 +1,8 @@
-const { packages } = require('./manifest');
+const packageFilter = (ignoredPrefixes) => {
+  return `^${ignoredPrefixes.map((prefix) => `(?!${prefix})`).join('')}.*$`;
+};
 
 module.exports = {
+  filter: packageFilter(['@airhelp\\/', 'eslint-config-airhelp-react']),
   source: ['package.json', 'packages/*/package.json', 'apps/*/package.json'],
-  semverGroups: [
-    {
-      range: '*',
-      dependencies: packages(),
-      packages: ['**'],
-    },
-  ],
 };
