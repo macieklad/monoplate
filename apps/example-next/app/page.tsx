96b8611de8afdf35e6fbfba63dfbb617bd2ac9ec
Maciej Ładoś <maciej@lados.dev>
2023-02-17 21:54:44 +0100

    feat: prefixed token example

--- apps/example-next/app/page.tsx
+++ apps/example-next/app/page.tsx
@@ -6,6 +6,6 @@ export default async function Page() {
   return <>
     <h1 className="text-blue-500 font-bold text-xl">Hello acme!</h1>
     <p>Your token list:</p>
-    {tokens.map((token, idx) => <p key={idx}>{token.title}</p>)}
+    {tokens.map((token, idx) => <p key={idx}>Prefixed: {token.title}</p>)}
   </>;
 }
